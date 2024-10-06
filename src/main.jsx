93e63e0cd67bd14cdd28419866885a3cc0aec7a7
AJAY GOUR <ajaygour3333@gmail.com>
2024-10-06 14:33:15 +0530

    Updated project to fix routing issues and build errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
